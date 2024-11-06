ae985a4289af35a443cf391ecd7468248174b42b
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-05 20:21:58 -0700

    finished exercise 19

--- solution.js
+++ solution.js
@@ -17,7 +17,7 @@ if (process.argv[1] === fileURLToPath(import.meta.url)) {
   */
 
   // Modify the line of code BELOW to run a different exercise
-  exercise_18();
+  exercise_19();
   // Modify the line of code ABOVE to run a different exercise
 }
 
@@ -483,12 +483,22 @@ export function exercise_19() {
     Exercise 19
     
     Date Arithmetic: 
+
+    1. Create a variable to hold a new Date object for today's date.
+    2. Create a variable to hold a new Date object for a future date in 2025
+    3. Find the time difference between the future date and the current date
+    4. Convert this time difference to days
+    5. Log the day difference to the console
 		
-		Calculate the difference in days between two Date objects and log the result. 
   */
+
+  // CODE IN THE OPEN LINES BELOW
+
   let currentDate = new Date();
-  let futureDate = new Date(2025, 0, 1); // January 1, 2025
+  let futureDate = new Date(2025, 0, 25); // January 25, 2025
   let timeDifference = futureDate - currentDate;
   let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  console.log(daysDifference); // Number of days until January 1, 2025
+  console.log(daysDifference); // Number of days until January 25, 2025
+
+  // CODE IN THE OPEN LINES ABOVE
 }
